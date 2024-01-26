7a59ee3e5441fd8b55060bade1b056dd6f9c15af
Luigi Pinca <luigipinca@gmail.com>
2024-01-26 18:31:26 +0100

    [pkg] Update ws to version 8.16.0

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ const { env } = process;
 const highWaterMark = +env.HIGH_WATER_MARK || 16384;
 
 const wss = new WebSocket.Server({
+  allowSynchronousEvents: true,
   maxPayload: +env.MAX_MESSAGE_SIZE || 64 * 1024,
   server
 });
